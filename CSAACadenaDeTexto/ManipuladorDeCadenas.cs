058f9377ea435084a4eb534ee2ce46acbb1225f5
CeciTaehyung <cecisabri123@gmail.com>
2024-10-10 16:46:46 -0600

    first commit

---
+++ CSAACadenaDeTexto/ManipuladorDeCadenas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAACadenaDeTexto
+{
+    public class ManipuladorDeCadenas
+    {
+        public string ConvertirAMayusculas(string input)
+        {
+            return input?.ToUpper();
+        }
+
+
+        public string EliminarEspacios(string input)
+        {
+            return input?.Replace(" ", "");
+        }
+    }
+}
